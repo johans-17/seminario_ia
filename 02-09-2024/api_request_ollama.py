c6a9633866a868eac14a4e10df1fdc0fcb95a4da
johan s-17 <johans.cortesi@ecci.edu.co>
2024-09-03 00:47:05 +0000

    json

--- 02-09-2024/api_request_ollama.py
+++ 02-09-2024/api_request_ollama.py
@@ -1,5 +1,6 @@
 
 import requests
+import json
 
 url = 'http://localhost:11434/api/generate'
 myobj = {
@@ -12,3 +13,9 @@ x = requests.post(url, json = myobj)
 
 print(x.text)
 
+
+# parse x:
+y = json.loads(x.text)
+
+# the result is a Python dictionary:
+print(y["response"])
